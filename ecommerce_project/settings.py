0d35f6d55ac9688c31b3a2215860424ab04e60c1
RakibulAzadd <rakibul.azad.hstu@example.com>
2025-04-04 10:27:18 +0600

    co

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '56fi&j$1yd33cf$1%tupzhlngah17l(*)n^h8p*oh6icv93(h='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['e-commerce-1-5zvv.onrender.com']
 
